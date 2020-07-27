3a1092877d9bd41af4bbb04e016729ceedf8cbc3
Ali Khaled <alikha1ed@outlook.com>
2020-07-27 15:15:47 +0200

    Updates

---
+++ precache-manifest.7e47028259ad802750af95467a6bd5d6.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "08c2e405d67333561d7545cfa3012da7",
+    "url": "/homefit-dashboard/index.html"
+  },
+  {
+    "revision": "aaf2cbf5cda91b389450",
+    "url": "/homefit-dashboard/static/css/2.18fb2ca0.chunk.css"
+  },
+  {
+    "revision": "a585c3f7b54bfb5cd544",
+    "url": "/homefit-dashboard/static/css/main.cbdf4783.chunk.css"
+  },
+  {
+    "revision": "aaf2cbf5cda91b389450",
+    "url": "/homefit-dashboard/static/js/2.dd7cfaef.chunk.js"
+  },
+  {
+    "revision": "286624c8d95769e3759a2a7869634d07",
+    "url": "/homefit-dashboard/static/js/2.dd7cfaef.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a585c3f7b54bfb5cd544",
+    "url": "/homefit-dashboard/static/js/main.ad1a26fd.chunk.js"
+  },
+  {
+    "revision": "e8f296e84f9edb5c6365",
+    "url": "/homefit-dashboard/static/js/runtime-main.c059d4fe.js"
+  }
+]);
